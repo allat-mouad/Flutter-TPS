ad8c9bf4b45e1d943a0e9a00eafc24a8474dcf96
allat mouad <80215500+moallat@users.noreply.github.com>
2022-04-07 21:18:21 +0000

    flutter app (contacts zone+github users zone+news zone+covid19 zone+ about zone)

---
+++ tp1_flutter/lib/news-api/article_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ArtcileView extends StatefulWidget {
+  const ArtcileView({Key? key}) : super(key: key);
+
+  @override
+  State<ArtcileView> createState() => _ArtcileViewState();
+}
+
+class _ArtcileViewState extends State<ArtcileView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
